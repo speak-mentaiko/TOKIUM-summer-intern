4acca86281a8fc1791b198d14d7491be5e4580c1
nelmo <hibiki.nemoto@gmail.com>
2024-08-22 14:35:16 +0900

    Added index method as create and show

--- app/controllers/api/v1/routes_controller.rb
+++ app/controllers/api/v1/routes_controller.rb
@@ -1,17 +1,19 @@
 class Api::V1::RoutesController < ApplicationController
   def index
-    if params[:data].present?
-      json_from_front = params.require(:data).permit!.to_h
-      processed_json = hogehoge(json_from_front)
-      render json: processed_json
+    if request.post?
+      if params[:params].present?
+        json_from_front = params.require(:data).permit!.to_h
+        render json:hogehoge(json_from_front)
+      else
+        render json: {error: 'parameter missing'}, status: :bad_request
+      end
     else
-      render json: {error: 'parameter missing'}, status: :bad_request
+      render json:{error: 'must to use post method'}, status: :bad_request
     end
+
   end
 
   private def hogehoge(json_from_front)
     json_from_front
   end
 end
-
-
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :hello, only: [:index]
       resources :routes, only: [:index]
+      post '/routes', to: 'routes#index'
     end
     
     namespace :v2 do
