e604bfc088fb7466380f35c65ca65dd01071ee96
naotookawa <na.okawa04@gmail.com>
2024-08-23 15:47:14 +0900

    check http

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -9,25 +9,31 @@ function App() {
     watchId: null,
     intervalId: null
   });
-  // const [loclist, setLoclist] = useState([])
-    let loclist = [];
+  const [fromTo,setFromTo] = useState({
+    "from": null,
+    "to": null
+  });
+  const [loclist, setLoclist] = useState([])
+    // useStateで問題が生じたらletでloclistを貼ればいいかも
 
     const startWatchPosition = () =>{
       if (!watchStatus.isWatching){
-        let lat, lon;
+        navigator.geolocation.getCurrentPosition(position => {
+          setLoclist([{longitude: position.coords.longitude, latitude: position.coords.latitude}])
+        });
 
+        let lon, lat;
       const watchId = navigator.geolocation.watchPosition(position => {   
-        lat = position.coords.latitude;
         lon = position.coords.longitude;
+        lat = position.coords.latitude;
       });
 
         const intervalId = setInterval(() => {
-          // setLoclist((prevLoclist) => {
-          //   return [...prevLoclist, 
-          //     {latitude: lat, longitude: lon}]
-          // });
-        loclist.push({latitude: lat, longitude: lon});
-        }, 10000); 
+          setLoclist((prevLoclist) => {
+            return [...prevLoclist, 
+              {longitude: lon, latitude: lat}]
+          });
+        }, 1000); 
 
       setWatchStatus({isWatching: true, watchId, intervalId });
         }
@@ -35,26 +41,30 @@ function App() {
 
     const stopWatchPosition = () => {
     if (watchStatus.isWatching) {
+      console.log(JSON.stringify(loclist))
+
       clearInterval(watchStatus.intervalId);
       navigator.geolocation.clearWatch(watchStatus.watchId);
       setWatchStatus({isWatching: false, watchId: null, intervalId: null});
 
-      fetch(`${API_BASE_URL}/route`,{
+      fetch(`${API_BASE_URL}/api/v1/routes`,{
         method : 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({
-          location : loclist
-        })
+        body: JSON.stringify(loclist)
       })
       .then(response => response.json())
-      // ユーザーidをつけるならここで追加
+      .then((data) => {
+        setFromTo(data);
+        console.log(data); 
+      })
+      .then(() => console.log(fromTo))
+      .catch(error => console.log(error)); 
 
-      // setLoclist ((prevLoclist) => {
-      //   return [];
-      // });
-      loclist = [];
+      setLoclist (() => {
+        return [];
+      });
       };
     };
 
@@ -62,6 +72,9 @@ function App() {
     <>
         <button onClick = {startWatchPosition}>位置情報取得開始</button>
         <button onClick = {stopWatchPosition}>位置情報取得終了</button>
+        <div>
+        </div>
+
     </>
   )
 }
