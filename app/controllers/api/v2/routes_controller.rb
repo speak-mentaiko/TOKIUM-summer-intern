30e5e411c3ca82bd67377a6b2b2a76ae061ec6ca
nelmo <hibiki.nemoto@gmail.com>
2024-08-23 17:38:36 +0900

    Added transfer detector and some typo fixing

--- app/controllers/api/v2/routes_controller.rb
+++ app/controllers/api/v2/routes_controller.rb
@@ -1,67 +1,88 @@
 require 'net/http'
 require 'json'
-require 'open-uri'
-require 'nokogiri'
-require 'uri'
 
 class Api::V2::RoutesController < ApplicationController
   def index
+    via_stations = []
     if request.post?
       data_arr = params[:_json]
       if data_arr.empty?
-        render json: {errors: "empty erroe"}
+        render json: {errors: "empty error"}
       end
       from = get_fromstation(data_arr)
       to = get_tostation(data_arr)
-      #cost = getCosts(from , to)
-      inf = {from: from, to: to}
+      course_data = detect_transfer(data_arr)
+
+      course_data.each do |route|
+        via_stations << _get_station_only_for_via(route)
+      end
+
+      if via_stations.empty?
+        inf = {from: from, to: to}
+      else
+        inf = {from: from, via: via_stations,to: to}
+      end
+
       if inf
         render json: inf, status: :created
       else
         render json: { errors: "error" }, status: :unprocessable_entity
       end
+
     elsif request.get?
       render json: { errors: "method error" }, status: :bad_request
     end
   end
 
   private
-
-  def move_distans(coordinates)
-    return _calc_distance(coordinates) / 1000
+  def detect_transfer(data_arr)
+    dist_arr = []
+    reduced_json = []
+    for i in 1..data_arr.length-3
+      dist_arr.push(_hubeny(data_arr[i]["longitude"], data_arr[i]["latitude"],data_arr[i+1]["longitude"],data_arr[i+1]["latitude"]))
+    end
+    i = 0
+    while i < dist_arr.length - 2
+      if dist_arr[i+1] <= 500
+        reduced_json << data_arr[i]
+        while dist_arr[i] <= 50
+          i += 1
+        end
+      else
+        i+=1
+      end
+    end
+    return reduced_json
   end
 
-  def _calc_distance(latlons)
-    latlons.map! { |latlon| _deg_to_rad(latlon) }
-    distance = (0...latlons.length - 1).inject(0) { |dist, i| dist + _hubeny(latlons[i], latlons[i + 1]) }
-    return distance
-  end
-
-  def _deg_to_rad(latlon)
-    latlon[:longitude] = latlon[:longitude] * Math::PI / 180
-    latlon[:latitude] = latlon[:latitude] * Math::PI / 180
-    return latlon
-  end
-
-  # http://yamadarake.jp/trdi/report000001.html
-  # WGS84 (GPS)
-  def _hubeny(latlng_1, latlng_2)
-    a = 6378137.000
-    b = 6356752.314245
-
-    pow_e = 1 - b ** 2 / a ** 2
-
-    ave = (latlng_1[:longitude] + latlng_2[:longitude]) / 2
-    dp = latlng_1[:longitude] - latlng_2[:longitude]
-    dr = latlng_1[:latitude] - latlng_2[:latitude]
-    sin_ave = Math.sin(ave)
-    cos_ave = Math.cos(ave)
-    w = Math.sqrt(1 - pow_e * sin_ave ** 2)
-    m = a * (1 - pow_e) / w ** 3
-    n = a / w
-
-    distance = Math.sqrt((m * dp) ** 2 + (n * cos_ave * dr) ** 2)
-    return distance
+# ヒュベニの公式
+  def _hubeny(lat1, lng1, lat2, lng2)
+    # 指定測地系の赤道半径・極半径を設定
+    lat1 *= Math::PI / 180.0
+    lat2 *= Math::PI / 180.0
+    lng1 *= Math::PI / 180.0
+    lng2 *= Math::PI / 180.0
+    r_x = 6378137.000000
+    r_y = 6356752.314245
+    # 2点の経度の差を計算 ( ラジアン )
+    a_x = lng1 - lng2
+    # 2点の緯度の差を計算 ( ラジアン )
+    a_y = lat1 - lat2
+    # 2点の緯度の平均を計算
+    p = ( lat1 + lat2 ) / 2.0
+    # 離心率を計算
+    e = Math::sqrt( ( r_x ** 2 - r_y ** 2 ) / ( r_x ** 2 ) )
+    # 子午線・卯酉線曲率半径の分母Wを計算
+    w = Math::sqrt( 1 - ( e ** 2 ) * ( ( Math::sin( p ) ) ** 2 ) )
+    # 子午線曲率半径を計算
+    m = r_x * ( 1 - e ** 2 ) / ( w ** 3 )
+    # 卯酉線曲率半径を計算
+    n = r_x / w
+    # 距離を計算
+    d = ( a_y * m ) ** 2
+    d += ( a_x * n * Math.cos( p ) ) ** 2
+    d = Math::sqrt( d )
+    return d
   end
 
   def get_fromstation(coordinates)
@@ -78,7 +99,29 @@ class Api::V2::RoutesController < ApplicationController
     # URI.parseは、URIオブジェクトを生成するメソッドです。
     http_client= Net::HTTP.new(uri.host,uri.port)
     # HTTPクライアントを生成し、引数にホスト名とポート番号を指定しています。
-    get_request = Net::HTTP::Get.new("/api/json?method=getStations&x=#{coordinate[:longitude]}&y=#{coordinate[:latitude]}", 'Content-Type' => 'application/json')
+    get_request = Net::HTTP::Get.new("/api/json?method=getStations&x=#{coordinate["longitude"]}&y=#{coordinate["latitude"]}", 'Content-Type' => 'application/json')
+    # Net::HTTP::Getは、HTTPのGETリクエストを表すクラスです。
+    # 引数にリクエストするpathとヘッダーを指定しています。
+    http_client.use_ssl = true
+    # httpsで通信をする場合はuse_sslをtrueにする必要がある
+    response = http_client.request(get_request)
+    # requestメソッドの引数にNet:HTTP:Responseオブジェクトをあたえます。
+    # responseには、HTTPレスポンスが格納されている
+    station_data = JSON.parse(response.body)
+    if station_data.empty?
+      return nil
+    else
+      # return _reform_json(station_data)
+      temp = _reform_json(station_data)
+      return temp
+    end
+  end
+  def _get_station_only_for_via(coordinate)
+    uri = URI.parse('https://express.heartrails.com')
+    # URI.parseは、URIオブジェクトを生成するメソッドです。
+    http_client= Net::HTTP.new(uri.host,uri.port)
+    # HTTPクライアントを生成し、引数にホスト名とポート番号を指定しています。
+    get_request = Net::HTTP::Get.new("/api/json?method=getStations&x=#{coordinate["longitude"]}&y=#{coordinate["latitude"]}", 'Content-Type' => 'application/json')
     # Net::HTTP::Getは、HTTPのGETリクエストを表すクラスです。
     # 引数にリクエストするpathとヘッダーを指定しています。
     http_client.use_ssl = true
@@ -97,12 +140,7 @@ class Api::V2::RoutesController < ApplicationController
   end
 
   def _reform_json(coodinates)
-    return coodinates["response"]["station"][0]["name"]
+    return coodinates = []? nil : ["response"]["station"][0]["name"]
   end
 
-  def getCosts(from, to)
-    doc = Nokogiri::HTML(open("https://www.jorudan.co.jp/norikae/cgi/nori.cgi?eki1=#{URI.encode_www_form(q: from)}&eki2=#{URI.encode_www_form(q: to)}&S=検索"))
-    cost = doc.xpath('//body/div[@id="container"]/div[@id="contents_out"]/div[@id="contentsdiv"]/div[@id="ain_wrapper"]/div[@id="js_routeBlocks"]/div[@id="data"]/div[@id="data_line_1"]/dl/dd/b')
-    return cost
-  end
 end
