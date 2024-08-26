8f0b7a5229ee05746fa1e4d136676d966cab3e65
nelmo <hibiki.nemoto@gmail.com>
2024-08-26 15:46:22 +0900

    first commit, added cost calc func and fixing some typo

--- app/controllers/api/v2/routes_controller.rb
+++ app/controllers/api/v2/routes_controller.rb
@@ -1,5 +1,8 @@
 require 'net/http'
 require 'json'
+require 'open-uri'
+require 'nokogiri'
+require 'uri'
 
 class Api::V2::RoutesController < ApplicationController
   def index
@@ -15,20 +18,23 @@ class Api::V2::RoutesController < ApplicationController
       course_data = detect_transfer(data_arr)
 
       course_data.each do |route|
-        temp = _get_station_only_for_via(route)
+        temp = _get_station(route)
         if !(via_stations.last == temp) && !(from == temp) && !(to == temp)
           via_stations << temp
         end
       end
+
+      distance = get_distance(data_arr)
 
       if via_stations.empty?
         inf = {from: from, to: to, amount: nil, distance: 0}
       elsif from.nil?
         inf = {error: "unprocessable request"}
       else
-        inf = {from: from, via: via_stations,to: to, amount: nil, distance: 0}
+        inf = {from: from, via: via_stations,to: to, amount: nil, distance: distance}
       end
-
+      costs = get_cost(from, via_stations, to)
+      inf = {from: from, to: to, amount: costs, distance: distance}
       render json: inf, status: :created
     else
       render json: { errors: "method error" }, status: :bad_request
@@ -57,35 +63,14 @@ class Api::V2::RoutesController < ApplicationController
     return Math::sqrt(( (lat1 - lat2) * 110574.275759 / ( w ** 3 ) ) ** 2 + ( (lng1 - lng2) *111319.49073 / w * Math.cos( p ) ) ** 2)
   end
   def get_fromstation(coordinates)
-    _get_station(coordinates, 0)
+    _get_station(coordinates[0])
   end
 
   def get_tostation(coordinates)
-    _get_station(coordinates, -1)
+    _get_station(coordinates[-1])
   end
-
-  def _get_station(coordinates, str)
-    coordinate = coordinates[str]
-    uri = URI.parse('https://express.heartrails.com')
-    # URI.parseは、URIオブジェクトを生成するメソッドです。
-    http_client= Net::HTTP.new(uri.host,uri.port)
-    # HTTPクライアントを生成し、引数にホスト名とポート番号を指定しています。
-    get_request = Net::HTTP::Get.new("/api/json?method=getStations&x=#{coordinate[:longitude]}&y=#{coordinate[:latitude]}", 'Content-Type' => 'application/json')
-    # Net::HTTP::Getは、HTTPのGETリクエストを表すクラスです。
-    # 引数にリクエストするpathとヘッダーを指定しています。
-    http_client.use_ssl = true
-    # httpsで通信をする場合はuse_sslをtrueにする必要がある
-    response = http_client.request(get_request)
-    # requestメソッドの引数にNet:HTTP:Responseオブジェクトをあたえます。
-    # responseには、HTTPレスポンスが格納されている
-    station_data = JSON.parse(response.body)
-    if station_data.empty?
-      return nil
-    else
-      return station_data["response"]["station"][0]["name"]
-    end
-  end
-  def _get_station_only_for_via(coordinate)
+  
+  def _get_station(coordinate)
     uri = URI.parse('https://express.heartrails.com')
     # URI.parseは、URIオブジェクトを生成するメソッドです。
     http_client= Net::HTTP.new(uri.host,uri.port)
@@ -102,14 +87,72 @@ class Api::V2::RoutesController < ApplicationController
     if station_data.empty?
       return nil
     else
-      # return _reform_json(station_data)
-      # sleep(1)
       return station_data["response"]["station"][0]["name"]
     end
   end
 
-  def _reform_json(coodinates)
-    return coodinates = []? nil : [0]
+  def get_distance(coordinates)
+    total_distance = 0
+    for i in 0..coordinates.length-2
+      total_distance = total_distance + _hubeny(coordinates[i]["latitude"], coordinates[i]["longitude"], coordinates[i+1]["latitude"], coordinates[i+1]["longitude"])
+    end
+    return (total_distance/1000).round(2)
   end
 
+  def get_cost(from, via_stations, to)
+    base_url = 'https://www.jorudan.co.jp/norikae/cgi/nori.cgi?'
+    from_station = from
+    to_station = to
+    if via_stations.empty?
+      via_station_num = 0
+    else
+      via_station_num = via_stations.length
+    end
+
+
+    case via_station_num-1
+    when -1
+      ad_url = "eki1=#{CGI.escapeHTML(from_station)}&eki2=#{CGI.escapeHTML(to_station)}&S=#{CGI.escapeHTML("検索")}"
+    when 0
+      ad_url = "eki1=" + URI.encode_www_form_component(from_station) +
+        "&eki2=" + URI.encode_www_form_component(to_station) +
+        "&via_on=" + (via_station_num - 1).to_s +
+        "&eki3=" + URI.encode_www_form_component(via_stations[0]) +
+        "&eki4=&eki5=&eki6=&Cway=0&Cfp=1&Czu=2&S=" + URI.encode_www_form_component("検索") + "&Csg=1"
+    when 1
+      ad_url = "eki1=" + URI.encode_www_form_component(from_station) +
+        "&eki2=" + URI.encode_www_form_component(to_station) +
+        "&via_on=" + (via_station_num - 1).to_s +
+        "&eki3=" + URI.encode_www_form_component(via_stations[0]) +
+        "&eki4=" + URI.encode_www_form_component(via_stations[1]) +
+        "&eki5=&eki6=&Cway=0&Cfp=1&Czu=2&S=" + URI.encode_www_form_component("検索") + "&Csg=1"
+
+    when 2
+      ad_url = "eki1=" + URI.encode_www_form_component(from_station) +
+        "&eki2=" + URI.encode_www_form_component(to_station) +
+        "&via_on=" + (via_station_num - 1).to_s +
+        "&eki3=" + URI.encode_www_form_component(via_stations[0]) +
+        "&eki4=" + URI.encode_www_form_component(via_stations[1]) +
+        "&eki5=" + URI.encode_www_form_component(via_stations[2]) +
+        "&eki6=&Cway=0&Cfp=1&Czu=2&S=" + URI.encode_www_form_component("検索") + "&Csg=1"
+    else
+      ad_url = "eki1=" + URI.encode_www_form_component(from_station) +
+        "&eki2=" + URI.encode_www_form_component(to_station) +
+        "&via_on=" + (via_station_num - 1).to_s +
+        "&eki3=" + URI.encode_www_form_component(via_stations[0]) +
+        "&eki4=" + URI.encode_www_form_component(via_stations[1]) +
+        "&eki5=" + URI.encode_www_form_component(via_stations[2]) +
+        "&eki6=" + URI.encode_www_form_component(via_stations[3]) +
+        "&Cway=0&Cfp=1&Czu=2&S=" + URI.encode_www_form_component("検索") + "&Csg=1"
+    end
+
+    url = (base_url + ad_url)
+    res = URI.open(url)
+    body = res.read
+    charset = res.charset
+    html = Nokogiri::HTML.parse(body, nil, charset)
+
+    s1 =html.css('div.data_line_1').css('dd').css('b')
+    return s1[0].text
+  end
 end
