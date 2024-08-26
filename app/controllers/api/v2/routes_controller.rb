00b863673bbc978fe296b5adb8173f9be73af8d1
nelmo <hibiki.nemoto@gmail.com>
2024-08-26 12:27:49 +0900

    Faster the _hubeny algorithm

--- app/controllers/api/v2/routes_controller.rb
+++ app/controllers/api/v2/routes_controller.rb
@@ -21,12 +21,12 @@ class Api::V2::RoutesController < ApplicationController
       end
 
       if via_stations.empty?
-        inf = {from: from, to: to, from_lad: data_arr[0]["latitude"], from_lon: data_arr[0]["longitude"]}
+        inf = {from: from, to: to, amount: nil, distance: 0}
       else
-        inf = {from: from, via: via_stations,to: to}
+        inf = {from: from, via: via_stations,to: to, amount: nil, distance: 0}
       end
 
-      if !(inf["from"].nil?) && !(inf["to"].nil?)
+      if 1
         render json: inf, status: :created
       else
         render json: { errors: "error" }, status: :unprocessable_entity
@@ -41,45 +41,23 @@ class Api::V2::RoutesController < ApplicationController
   def detect_transfer(data_arr)
     key_dist = 100
     reduced_json = []
+    total_distance = 0
     for i in 1..data_arr.length-3
       reduced_json << data_arr[i]
       while _hubeny(data_arr[i]["latitude"], data_arr[i]["longitude"],data_arr[i+1]["latitude"],data_arr[i+1]["longitude"]) <= key_dist && i <= data_arr.length-3
         i += 1
       end
     end
+
     return reduced_json
   end
 
 # ヒュベニの公式
   def _hubeny(lat1, lng1, lat2, lng2)
-    # 指定測地系の赤道半径・極半径を設定
-    lat1 *= Math::PI / 180.0
-    lat2 *= Math::PI / 180.0
-    lng1 *= Math::PI / 180.0
-    lng2 *= Math::PI / 180.0
-    r_x = 6378137.000000
-    r_y = 6356752.314245
-    # 2点の経度の差を計算 ( ラジアン )
-    a_x = lng1 - lng2
-    # 2点の緯度の差を計算 ( ラジアン )
-    a_y = lat1 - lat2
-    # 2点の緯度の平均を計算
-    p = ( lat1 + lat2 ) / 2.0
-    # 離心率を計算
-    e = Math::sqrt( ( r_x ** 2 - r_y ** 2 ) / ( r_x ** 2 ) )
-    # 子午線・卯酉線曲率半径の分母Wを計算
-    w = Math::sqrt( 1 - ( e ** 2 ) * ( ( Math::sin( p ) ) ** 2 ) )
-    # 子午線曲率半径を計算
-    m = r_x * ( 1 - e ** 2 ) / ( w ** 3 )
-    # 卯酉線曲率半径を計算
-    n = r_x / w
-    # 距離を計算
-    d = ( a_y * m ) ** 2
-    d += ( a_x * n * Math.cos( p ) ) ** 2
-    d = Math::sqrt( d )
-    return d
+    p = ( lat1 + lat2 ) * 0.00872664625
+    w = Math::sqrt( 1 - (0.00669437998  * ( ( Math::sin( p ) ) ** 2 )) )
+    return Math::sqrt(( (lat1 - lat2) * 110574.275759 / ( w ** 3 ) ) ** 2 + ( (lng1 - lng2) *111319.49073 / w * Math.cos( p ) ) ** 2)
   end
-
   def get_fromstation(coordinates)
     _get_station(coordinates, 0)
   end
