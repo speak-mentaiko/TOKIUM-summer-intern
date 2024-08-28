46c11e72c58f2b58decba1c00a2448fe080c9c06
nelmo <hibiki.nemoto@gmail.com>
2024-08-28 13:29:23 +0900

    added exception exec in all of v2/costs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, on: :create
+  validates :email, presence: true, uniqueness: true, on: :create
 end
