311207bc5de2aa32891786b1d77284a62470ea28
root <root@Nelmo-FLOW-X13>
2024-08-28 09:39:59 +0900

    Revert "Fixing missed files"
    This reverts commit 55b30dc2cedac60441d5c37cf70dc6b8115e1d56.

--- db/migrate/20240827063247_add_column_users.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnUsers < ActiveRecord::Migration[7.0]
-  def up
-    add_column :routes, :user_id, :string
-  end
-  def down
-    remove_column :routes, :user_id
-  end
-end
--- front/src/pages/test.tsx
+++
@@ -1,7 +0,0 @@
-export const Test = () => {
-  return (
-    <>
-      <p>test</p>
-    </>
-  );
-};
