7b03e95db203b2bc577a995164558048ed0ae46d
nelmo <hibiki.nemoto@gmail.com>
2024-08-29 11:45:53 +0900

    added css in signin and signup

--- front/src/components/Signup.tsx
+++ front/src/components/Signup.tsx
@@ -50,37 +50,62 @@ export const Signup = () => {
   };
 
   return (
-    <>
-      <form onSubmit={signUpSubmit}>
-        <label>
-          メールアドレス:
-          <input type="email" name="email" defaultValue="" />
-        </label>
-        <label>
-          パスワード:
-          <input type="password" name="password" defaultValue="" />
-        </label>
-        <label>
-          パスワード(もう一度):
-          <input type="password" name="password_conf" defaultValue="" />
-        </label>
-        <label>
-          名前:
-          <input type="text" name="name" defaultValue="" />
-        </label>
-        <label>
-          プロジェクト:
-          <input type="text" name="project" defaultValue="" />
-        </label>
-        <label>
-          部署:
-          <input type="text" name="part" defaultValue="" />
-        </label>
-        <button>
-          <input type="submit" value="Submit" />
-        </button>
-      </form>
-      <p>{error}</p>
-    </>
-  );
+      <>
+        <section className="bg-gray-50 dark:bg-gray-900">
+          <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <div
+                className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+              <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                  Sign up to your new account
+                </h1>
+                <form className="space-y-4 md:space-y-6" action="#" onSubmit={signUpSubmit}>
+                  <div>
+                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
+                      email</label>
+                    <input type="email" name="email" id="email"
+                           className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                           defaultValue="" required="1"/>
+                  </div>
+                  <div>
+                    <label htmlFor="password"
+                           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                    <input type="password" name="password" id="password" placeholder="••••••••"
+                           className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                           required="1"/>
+                  </div>
+                  <div>
+                    <label htmlFor="password_confirm"
+                           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password_confirm</label>
+                    <input type="password" name="password_confirm" id="password_confirm" placeholder="••••••••"
+                           className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                           required="1"/>
+                  </div>
+                  <div>
+                    <label htmlFor="name"
+                           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Name</label>
+                    <input type="text" name="name" id="name" placeholder=""
+                           className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                           required="1"/>
+                  </div>
+                  <div>
+                    <label htmlFor="project"
+                           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Project name</label>
+                    <input type="text" name="project" id="project" placeholder=""
+                           className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                           required="1"/>
+                  </div>
+                  <button type="submit"
+                          className="w-full text-white bg-blue-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center cursor-pointer outline-none">Sign
+                    up
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </section>
+
+      </>
+  )
+      ;
 };
